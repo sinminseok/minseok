52f2f239b86df15e350482404cde55a49103ff9c
sinminseok12 <sinminseok12@naver.com>
2024-05-13 19:29:28 +0900

    Initial commit

---
+++ src/main/java/study/minseok/MinseokApplication.java
@@ -0,0 +1,13 @@
+package study.minseok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinseokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinseokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/minseok/MinseokApplicationTests.java
@@ -0,0 +1,13 @@
+package study.minseok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinseokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
